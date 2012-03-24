dcffce9a236bba8b0d65f29370fdd142b4851318
Corey Woodcox <corey.woodcox@gmail.com>
2012-03-23 23:52:58 -0600

    WIP rackspace clouddb support

--- lib/fog/rackspace/cloud_db.rb
+++ lib/fog/rackspace/cloud_db.rb
@@ -4,6 +4,17 @@ module Fog
   module Rackspace
     class CloudDB < Fog::Service
 
+      requires :rackspace_api_key, :rackspace_username
+      recognizes :rackspace_auth_url, :persistent
+
+      model_path 'fog/rackspace/models/cloud_db'
+
+      request_path 'fog/rackspace/requests/cloud_db'
+
+      request :create_db_instance
+      request :modify_db_instance
+      request :describe_db_instance
+      request :describe_db_instances
     end
   end 
 end
